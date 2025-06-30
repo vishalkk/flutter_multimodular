92200cbb70ab25e5ff9b89a1f21546ca71eb2e20
VISHAL KRISHNA KAJALE <vishalkajales@gmail.com>
2025-06-30 17:27:28 +0530

    learn access of module from other modules.

--- features/settings/profile/lib/profile.dart
+++ features/settings/profile/lib/profile.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_multimodular_practice/main_app_provider.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  var name = getAppName();
 }
