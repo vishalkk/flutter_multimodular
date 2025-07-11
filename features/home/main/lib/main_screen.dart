c4bc1a6ce1412e65eee00521a14988de14fe3899
VISHAL KRISHNA KAJALE <vishalkajales@gmail.com>
2025-07-11 13:38:50 +0530

    added bottom nav bar

--- features/home/main/lib/main_screen.dart
+++ features/home/main/lib/main_screen.dart
@@ -1,4 +1,10 @@
+import 'package:app_settings/settings_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:main/main_event.dart';
+import 'package:main/main_screen_bloc.dart';
+import 'package:main/main_state.dart';
+import 'package:movies/movies_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,99 +14,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _screens = [MoviesScreen(), SettingsScreen()];
   final List<String> items = List.generate(20, (index) => 'Item ${index + 1}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              // Add search functionality here
-              print('Search button pressed');
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.notifications),
-            onPressed: () {
-              // Add notification functionality here
-              print('Notification button pressed');
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text(
-                'Menu',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-            ListTile(
-              title: const Text('Profile'),
-              leading: const Icon(Icons.account_circle),
-              onTap: () {
-                // Add profile functionality here
-                print('Profile tapped');
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Settings'),
-              leading: const Icon(Icons.settings),
-              onTap: () {
-                // Add settings functionality here
-                print('Settings tapped');
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Logout'),
-              leading: const Icon(Icons.logout),
-              onTap: () {
-                // Add logout functionality here
-                print('Logout tapped');
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        title: BlocBuilder<MainScreenBloc, MainScreenState>(
+          builder: (context, state) {
+            return Text(
+              state.selectedIndex == 0 ? 'Movies' : 'Settings',
+              style: const TextStyle(fontSize: 20),
+            );
+          },
         ),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(items[index]),
-                leading: const Icon(Icons.label),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {
-                  // Add item tap functionality here
-                  print('Item $index tapped');
-                },
-              );
-            },
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add floating action button functionality here
-          print('Floating Action Button pressed');
+      body: BlocBuilder<MainScreenBloc, MainScreenState>(
+        builder: (context, state) {
+          return _screens[state.selectedIndex];
         },
-        tooltip: 'Add',
-        child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+      bottomNavigationBar: BlocBuilder<MainScreenBloc, MainScreenState>(
+        builder: (context, state) {
+          return BottomNavigationBar(
+            currentIndex: state.selectedIndex,
+            onTap: (index) {
+              context.read<MainScreenBloc>().add(ChangeTab(index));
+            },
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.movie), label: 'Movies'),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
---
+++ features/settings/profile/lib/profile_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Text(
+        'Profile Screen',
+        style: TextStyle(color: Colors.black, fontSize: 24),
+      ),
+    );
+  }
+}
